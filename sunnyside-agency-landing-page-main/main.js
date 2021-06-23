5a7e1df64d0c4b0fd3ebb57b326956ce1dc78524
WebLytic <73708874+WebLytic@users.noreply.github.com>
2021-06-23 20:24:01 +0500

    Delete sunnyside-agency-landing-page-main directory

--- sunnyside-agency-landing-page-main/main.js
+++
@@ -1,6 +0,0 @@
-const hamIcon = document.querySelector(".ham-icon");
-const navMenu = document.querySelector(".nav-ul");
-
-hamIcon.addEventListener('click', function() {
-    navMenu.classList.toggle("nav-mob");
-});
